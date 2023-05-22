b2af887b6af16fed59fcad5df84ad224fa96c74f
austinbratcher <dev@austinbratcher.com>
2023-05-21 20:29:06 -0500

    feat: work on multiple Mac chip architectures

--- utils.ts
+++ utils.ts
@@ -1,4 +1,17 @@
 import Logger from "https://deno.land/x/logger@v1.1.1/logger.ts";
+import {Config} from "./types.ts";
+
+const logger = new Logger();
+const logPath = Deno.env.get("HOME") + "/.rebrowse.log";
+await logger.initFileLogger(logPath, {
+    rotate: true,
+});
+
+logger.disableConsole();
+
+export {
+    logger,
+}
 
 export const ReadJsonFileSync = <T>(filePath: string): T => {
     const rawJSON = Deno.readTextFileSync(filePath);
@@ -6,11 +19,15 @@ export const ReadJsonFileSync = <T>(filePath: string): T => {
 }
 
 
-const logger = new Logger();
-const logPath = Deno.env.get("HOME") + "/.rebrowse.log";
-await logger.initFileLogger(logPath);
-logger.disableConsole();
+export const ReadConfigFileSync = (): Config => {
+    const configPath = Deno.env.get("HOME") + "/.rebrowse";
+    try {
+        return ReadJsonFileSync<Config>(configPath);
+    } catch (e) {
+        logger.error("Error reading config", e);
+    }
 
-export {
-    logger,
+    return {
+        url_patterns: [],
+    };
 }
